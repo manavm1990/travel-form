8499175d2f2b59361a3c2fb7a5fc81e92f7b2b43
Manav Misra <manavm@visionwebsoft.com>
2020-08-24 13:44:13 -0500

    :art: Format components

--- src/components/Form/Inputs/Checkbox.js
+++ src/components/Form/Inputs/Checkbox.js
@@ -1,10 +1,16 @@
 import PropTypes from "prop-types"
 import React from "react"
 
-export const Checkbox = ({checked, id, label, handler}) => {
+export const Checkbox = ({ checked, id, label, handler }) => {
   return (
     <label className="checkbox">
-      <input checked={checked} id={id} onChange={handler} type="checkbox" className="mr-1" />
+      <input
+        checked={checked}
+        id={id}
+        onChange={handler}
+        type="checkbox"
+        className="mr-1"
+      />
       {label}
     </label>
   )
@@ -14,5 +20,5 @@ Checkbox.propTypes = {
   checked: PropTypes.bool,
   handler: PropTypes.func,
   id: PropTypes.string.isRequired,
-  label: PropTypes.string.isRequired
+  label: PropTypes.string.isRequired,
 }
--- src/components/Form/Inputs/Input.js
+++ src/components/Form/Inputs/Input.js
@@ -1,26 +1,22 @@
-import PropTypes from "prop-types";
-import React from "react";
+import PropTypes from "prop-types"
+import React from "react"
 
-export const Input = ({id, placeholder, type, handler}) => {
-  return (
-    <div className="field">
-      <label className="label" htmlFor={id}>{placeholder}</label>
-      <div className="control">
-        <input
-          type={type}
-          id={id}
-          onChange={handler}
-        />
-      </div>
+export const Input = ({ id, placeholder, type, handler }) => (
+  <div className="field">
+    <label className="label" htmlFor={id}>
+      {placeholder}
+    </label>
+    <div className="control">
+      <input type={type} id={id} onChange={handler} />
     </div>
-  )
-}
+  </div>
+)
 
 Input.propTypes = {
   handler: PropTypes.func,
   id: PropTypes.string.isRequired,
   placeholder: PropTypes.string,
-  type: PropTypes.string
+  type: PropTypes.string,
 }
 
 Input.defaultProps = { type: "text" }

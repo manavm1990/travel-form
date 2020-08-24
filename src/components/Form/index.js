f93ee148f0bc386a4fea64fbc75b5fda0d6cfc03
Manav Misra <manavm@visionwebsoft.com>
2020-08-24 14:06:29 -0500

    :construction: Add error states

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -5,8 +5,11 @@ import { Select } from "./Select"
 
 export const Form = () => {
   const [firstName, setFirstName] = useState("")
+  const [firstNameError, setFirstNameError] = useState("")
   const [lastName, setLastName] = useState("")
+  const [lastNameError, setLastNameError] = useState("")
   const [email, setEmail] = useState("")
+  const [emailError, setEmailError] = useState("")
   const [gender, setGender] = useState("")
   const [destination, setDestination] = useState("India")
   const [isVegan, setIsVegan] = useState(false)

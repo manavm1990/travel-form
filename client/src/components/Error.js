f9123ec880ce2e3901bc8df29feba81e041d221e
Manav Misra <manavm@visionwebsoft.com>
2020-08-25 15:25:01 -0500

    :lipstick: Use Bulma to style error page

--- client/src/components/Error.js
+++ client/src/components/Error.js
@@ -3,10 +3,15 @@ import React from "react"
 import { useLocation } from "react-router-dom"
 
 export const Error = () => {
+
   const {
     state: {
       error: { name, message },
     },
   } = useLocation()
-  return <p>Currently facing issue! {name} - {message}</p>
+  return (
+    <p className="has-text-danger">
+      Currently facing issue! {name} - {message}
+    </p>
+  )
 }

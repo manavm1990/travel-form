0718de8abee401990390ada3c98be5fc357450e4
Manav Misra <manavm@visionwebsoft.com>
2020-08-24 22:56:11 -0500

    Add login link button
    Updating colors.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react"
 
 export const Header = () => (
-         <header className="hero is-primary">
-           <div className="hero-body">
-             <div className="container">
-               <h1 className="has-text-centered title">Register for Travel</h1>
-             </div>
-           </div>
-         </header>
-       )
+  <header className="hero has-background-light">
+    <div className="hero-body">
+      <div className="container has-text-centered ">
+        <h1 className="title">Register for Travel</h1>
+        <button className="button is-link">Login</button>
+      </div>
+    </div>
+  </header>
+)

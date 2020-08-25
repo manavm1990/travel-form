52c8823fa86d83f10636978f4a8595ba5e2955d4
Manav Misra <manavm@visionwebsoft.com>
2020-08-25 15:27:14 -0500

    :lipstick: Style success page

--- client/src/components/Success.js
+++ client/src/components/Success.js
@@ -1,9 +1,9 @@
 import PropTypes from "prop-types"
 import React from "react"
 
-export const Success = ({ location: { message } }) => {
-  return <p>Success! {message}</p>
-}
+export const Success = ({ location: { message } }) => (
+  <p className="has-text-success">Success! {message}</p>
+)
 
 Success.propTypes = {
   location: PropTypes.shape({ message: PropTypes.string }),

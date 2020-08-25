18e80f936b879ff436f4677332241662a9f784b7
Manav Misra <manavm@visionwebsoft.com>
2020-08-25 14:36:28 -0500

    :lipstick: Add more data and bulma-fy table

--- client/src/components/LoginTable/Table.js
+++ client/src/components/LoginTable/Table.js
@@ -1,12 +1,29 @@
 import PropTypes from "prop-types"
 import React from "react"
 
-export const Table = ({ travels }) => {
-  return (
-    <ul>
-      {travels.map(({ destination }, i) => <li key={i}>{destination}</li>)}
-    </ul>
-  )
-}
+export const Table = ({ travels }) => (
+  <table className="table">
+    <thead>
+      <tr>
+        <th>First Name</th>
+        <th>Last Name</th>
+        <th>Vegan?</th>
+        <th>Destination</th>
+      </tr>
+    </thead>
+    <tbody>
+      {travels.map(
+        ({ firstName, lastName, dietRestrictions, destination }, i) => (
+          <tr key={i}>
+            <td>{firstName}</td>
+            <td>{lastName}</td>
+            <td>{dietRestrictions.isVegan ? "Y" : "N"}</td>
+            <td>{destination}</td>
+          </tr>
+        )
+      )}
+    </tbody>
+  </table>
+)
 
 Table.propTypes = { travels: PropTypes.array.isRequired }

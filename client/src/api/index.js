1511e3cc45cf49c4aded7286c7d38defc92bd24b
Manav Misra <manavm@visionwebsoft.com>
2020-08-25 14:45:03 -0500

    :goal_net: Add try-catch to add travels

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,16 +1,17 @@
 export default {
   async addTravel(newTravel) {
-    const res = await fetch("http://localhost:5000/travel/add", {
-      method: "POST",
-      headers: {
-        // Must specify that we are sending JSON! 🙄
-        "Content-Type": "application/json",
-      },
-      // Turn 'newTravel' JS Object into actual JSON
-      body: JSON.stringify(newTravel),
-    })
-    return await res.json()
-  },
+    try {
+      const res = await fetch("http://localhost:5000/travel/add", {
+        method: "POST",
+        headers: {
+          // Must specify that we are sending JSON! 🙄
+          "Content-Type": "application/json",
+        },
+        // Turn 'newTravel' JS Object into actual JSON
+        body: JSON.stringify(newTravel),
+      })
+      return await res.json()
+    } catch (e) {
 
   async deleteAllTravels() {
     const res = await fetch("http://localhost:5000/travel/delete", {

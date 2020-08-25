5da81affd3f5a9f796ee8efb2bbdf35042040388
Manav Misra <manavm@visionwebsoft.com>
2020-08-25 09:50:50 -0500

    Use router to link to home and render form/travel list

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,5 @@
 import React from "react"
-import {Link, useLocation} from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 
 export const Header = () => {
   const location = useLocation()
@@ -8,8 +8,14 @@ export const Header = () => {
     <header className="hero has-background-light">
       <div className="hero-body">
         <div className="container has-text-centered ">
-          <h1 className="title">Register for Travel</h1>
-          {location.pathname === "/" ? <Link className="button is-link" to="/login">Login</Link> : null}
+          <Link to="/">
+            <h1 className="title">Register for Travel</h1>
+          </Link>
+          {location.pathname === "/" ? (
+            <Link className="button is-link" to="/login">
+              Login
+            </Link>
+          ) : null}
         </div>
       </div>
     </header>
